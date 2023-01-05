9226c38930593cb7c47dc245d72f3896b47be64d
mpaul97 <michaelandrewpaul97@gmail.com>
2023-01-05 15:56:04 -0600

    working

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -14,8 +14,12 @@ export class BoardComponent implements OnInit {
 
   pieces: Info[] = [];
 
-  started: boolean = false;
+  selectedPiece: Info = new Info();
 
+  started: boolean = false;
+  isWhiteMove: boolean = true;
+
+  allSpaces: Space[] = [];
   playableSpaces: Space[] = [];
 
   constructor() {}
@@ -23,29 +27,94 @@ export class BoardComponent implements OnInit {
   ngOnInit(): void {
     this.initBlackPieces();
     this.initWhitePieces();
+    this.initAllSpaces();
   }
 
   spaceClicked(file: string, rank: number) {
     let piece = this.getPiece(file, rank);
     if (piece) {
       this.findMoves(piece);
+      this.selectedPiece = piece;
+    } 
+    let isSpacePlayable = this.playableSpaces.filter(x => x.file === file && x.rank === rank)[0];
+    if (isSpacePlayable) {
+      let index = this.pieces.findIndex(x => x.rank === this.selectedPiece.rank && x.file === this.selectedPiece.file);
+      let pieceSpaceIndex = this.allSpaces.findIndex(x => x.rank === this.selectedPiece.rank && x.file === this.selectedPiece.file);
+      let clickedSpaceIndex = this.allSpaces.findIndex(x => x.rank === rank && x.file === file);
+      if (index) { // clicked space is playable
+        this.pieces[index].rank = rank;
+        this.pieces[index].file = file;
+        this.pieces[index].hasMoved = true;
+        this.isWhiteMove = !this.isWhiteMove;
+        this.playableSpaces = [];
+        this.allSpaces[pieceSpaceIndex].hasPiece = false; // old space
+        this.allSpaces[clickedSpaceIndex].hasPiece = true; // new space
+      };
     }
   }
 
   findMoves(piece: Info) {
-    if (!this.started) {
+    if (this.isWhiteMove) { // white to move
       if (piece.isWhite()) {
-        if (piece.isPawn()) {
-          if (!piece.hasMoved) {
-            this.playableSpaces = [new Space(piece.file, piece.rank + 1), new Space(piece.file, piece.rank + 2)];
-          } else {
-            this.playableSpaces = [new Space(piece.file, piece.rank + 1)];
-          }
-        } else if (piece.isKnight()) {
-          this.playableSpaces = [new Space(this.files[this.getFileIndex(piece.file) + 1], piece.rank + 2), new Space(this.files[this.getFileIndex(piece.file) - 1], piece.rank + 2)];
-        }
+        this.getPlayableSpaces(piece);
+      }
+    } else { // black to move
+      if (!piece.isWhite()) {
+        this.getPlayableSpaces(piece);
       }
     }
+  }
+
+  getPlayableSpaces(piece: Info) {
+    if (piece.isPawn()) {
+      if (piece.isWhite()) {
+        if (!piece.hasMoved) {
+          this.playableSpaces = [new Space(piece.file, piece.rank + 1), new Space(piece.file, piece.rank + 2)];
+        } else {
+          this.playableSpaces = [new Space(piece.file, piece.rank + 1)];
+        }
+      } else {
+        if (!piece.hasMoved) {
+          this.playableSpaces = [new Space(piece.file, piece.rank - 1), new Space(piece.file, piece.rank - 2)];
+        } else {
+          this.playableSpaces = [new Space(piece.file, piece.rank - 1)];
+        }
+      }
+    } else if (piece.isKnight()) {
+      this.playableSpaces = this.getKnightMoves(piece.file, piece.rank);
+    } else if (piece.isBishop()) {
+      this.playableSpaces = this.getBishopMoves(piece.file, piece.rank);
+    }
+  }
+
+  getKnightMoves(file: string, rank: number) : Space[] {
+    return [
+      new Space(this.files[this.getFileIndex(file) + 1], rank + 2),
+      new Space(this.files[this.getFileIndex(file) - 1], rank + 2),
+      new Space(this.files[this.getFileIndex(file) - 2], rank + 1),
+      new Space(this.files[this.getFileIndex(file) + 2], rank + 1),
+      new Space(this.files[this.getFileIndex(file) + 1], rank - 2),
+      new Space(this.files[this.getFileIndex(file) - 1], rank - 2),
+      new Space(this.files[this.getFileIndex(file) - 2], rank - 1),
+      new Space(this.files[this.getFileIndex(file) + 2], rank - 1)
+    ]
+  }
+
+  getBishopMoves(file: string, rank: number) : Space[] {
+    let spaces: Space[] = [];
+    for (var i = 1; i < 8; i++) {
+      let targetFile = this.files[this.getFileIndex(file) + i];
+      let targetRank = rank + i;
+      let allSpacesIndex = this.allSpaces.indexOf(x => x.file === targetFile && x.rank === targetRank);
+      if (this.allSpaces[allSpacesIndex].hasPiece) {
+
+      }
+      spaces.push(new Space(targetFile, targetRank);
+    }
+    // return [
+    //   new Space(this.files[this.getFileIndex(file) + 1], rank + 1)
+    // ]
+    return spaces
   }
 
   getFileIndex(file: string) {
@@ -101,6 +170,18 @@ export class BoardComponent implements OnInit {
     this.pieces.push(new Info('white', 'queen', 'd', 1));
     // king
     this.pieces.push(new Info('white', 'king', 'e', 1));
+  }
+
+  initAllSpaces() {
+    this.files.forEach((file, y) => {
+      this.ranks.forEach((rank, x) => {
+        if (this.getPiece(file, rank)) {
+          this.allSpaces.push(new Space(file, rank, true, x, y));
+        } else {
+          this.allSpaces.push(new Space(file, rank, false, x, y));
+        }
+      })
+    })
   }
 
 }
--- src/models/space.ts
+++ src/models/space.ts
@@ -1,6 +1,9 @@
 export class Space {
   constructor(
     public file: string = '',
-    public rank: number = 0
+    public rank: number = 0,
+    public hasPiece: boolean = false,
+    public x: number = 0,
+    public y: number = 0
   ) {}
 }
